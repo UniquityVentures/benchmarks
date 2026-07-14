97bcdabea6dadb001cbcffe0c321a4de4e6dd490
Sandeep Kumar <sandeepqmar05@gmail.com>
2026-07-14 15:19:51 +0530

    Fixed async sync wsgi problem

--- django-benchmark/run_wsgi.sh
+++ django-benchmark/run_wsgi.sh
@@ -4,5 +4,5 @@ echo "Starting Django using WSGI (Gunicorn)..."
 exec uv run gunicorn config.wsgi:application \
   --bind 0.0.0.0:8125 \
   --workers 4 \
-  --worker-class gevent \
-  --worker-connections 1000
+  --worker-class gthread \
+  --threads 10
